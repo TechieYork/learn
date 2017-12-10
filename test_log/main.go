97f64a65399a4d9cd3d152ec6a97c470a845a9c7
york528 <york528@yeah.net>
2017-12-11 01:59:08 +0800

    feat:Update test log

--- test_log/main.go
+++ test_log/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"fmt"
 
-
+	logrus "github.com/sirupsen/logrus"
+	//blog4go "github.com/YoungPioneers/blog4go"
+	//seelog "github.com/cihub/seelog"
 )
 
 func testLogrus() {
-
+	fmt.Println("====== Test logrus ======")
+	logrus.Infoln("test logrus")
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 }
 
 func testBlog4go() {
-
+	fmt.Println("====== Test blog4go ======")
 }
 
 func testSeelog() {
-
+	fmt.Println("====== Test seelog ======")
 }
 
 func main() {
+	fmt.Println("====== Test log ======")
 
+	testSeelog()
+	testBlog4go()
+	testLogrus()
 }
