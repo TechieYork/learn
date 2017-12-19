9882f2c98792753bd34d048d2e7f3bed73a660dd
techieliu(刘超) <techieliu@tencent.com>
2017-12-19 17:15:27 +0800

    feat:Finish client and server test code

--- test_grpc/basic/client/main.go
+++ test_grpc/basic/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "learn/test_grpc/basic/proto"
+)
 
 func main() {
-	fmt.Println("test")
+	// 连接
+	conn, err := grpc.Dial(":58888", grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	// 初始化客户端
+	c := pb.NewUserServiceClient(conn)
+
+	// 调用方法
+	req := new(pb.AddUserRequest)
+	req.Session = new(pb.Session)
+	req.User = new(pb.User)
+
+	req.Session.Seq = "12345678"
+	req.User.Name = "techieliu"
+	req.User.Sex = 1
+
+	resp, err := c.AddUser(context.Background(), req)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(resp.String())
 }
--- test_grpc/basic/server/main.go
+++ test_grpc/basic/server/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "learn/test_grpc/basic/proto"
+)
+
+// 定义UserService并实现约定的接口
+type UserService struct{}
+
+// UserService ...
+var userService = UserService{}
+
+func (h UserService) AddUser (ctx context.Context, req *pb.AddUserRequest) (*pb.AddUserResponse, error) {
+	fmt.Println(req.String())
+
+	resp := new(pb.AddUserResponse)
+	resp.Session = new(pb.Session)
+	resp.Result = new(pb.Result)
+
+	//Set session
+	resp.Session = req.Session
+
+	//Set result
+	resp.GetResult().Code = 0
+	resp.GetResult().Message = "Success"
+
+	return resp, nil
+}
 
 func main() {
-	fmt.Println("test")
+	listen, err := net.Listen("tcp", ":58888")
+	if err != nil {
+		fmt.Println("failed to listen: ", err.Error())
+		return
+	}
+
+	// 实例化grpc Server
+	s := grpc.NewServer()
+
+	// 注册HelloService
+	pb.RegisterUserServiceServer(s, userService)
+
+	fmt.Println("Listen on :58888")
+
+	s.Serve(listen)
 }
