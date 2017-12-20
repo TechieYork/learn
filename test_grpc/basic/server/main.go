c3b34454bc51e1506492564d119b0245f385f09d
techieliu(刘超) <techieliu@tencent.com>
2017-12-20 10:21:23 +0800

    feat:Modify some print

--- test_grpc/basic/server/main.go
+++ test_grpc/basic/server/main.go
@@ -10,15 +10,13 @@ import (
 	pb "learn/test_grpc/basic/proto"
 )
 
-// 定义UserService并实现约定的接口
+// Implement UserService interface
 type UserService struct{}
 
 // UserService ...
 var userService = UserService{}
 
 func (h UserService) AddUser (ctx context.Context, req *pb.AddUserRequest) (*pb.AddUserResponse, error) {
-	fmt.Println(req.String())
-
 	resp := new(pb.AddUserResponse)
 	resp.Session = new(pb.Session)
 	resp.Result = new(pb.Result)
@@ -34,16 +32,48 @@ func (h UserService) AddUser (ctx context.Context, req *pb.AddUserRequest) (*pb.
 }
 
 func main() {
+	// start
+	fmt.Println("====== Test grpc server ======")
+
 	listen, err := net.Listen("tcp", ":58888")
 	if err != nil {
 		fmt.Println("failed to listen: ", err.Error())
 		return
 	}
 
-	// 实例化grpc Server
-	s := grpc.NewServer()
+	var opts []grpc.ServerOption
 
-	// 注册HelloService
+	// interceptor
+	var interceptor grpc.UnaryServerInterceptor
+	interceptor = func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		fmt.Println("====== Enter interceptor ======")
+		fmt.Printf("context:\r\n%v\r\n", ctx)
+		fmt.Printf("method:\r\n%v\r\n", info.FullMethod)
+		fmt.Printf("server:\r\n%v\r\n", info.Server)
+
+		// 继续处理请求
+		fmt.Println("====== interceptor handle ======")
+		fmt.Printf("req:%v\r\n", req)
+
+		resp, err = handler(ctx, req)
+
+		if err != nil {
+			fmt.Printf("handle failed! error:%v\r\n", err)
+		} else {
+			fmt.Printf("handle success! resp:%v\r\n", resp)
+		}
+
+		fmt.Println("====== Leave interceptor ======")
+
+		return resp, err
+	}
+
+	opts = append(opts, grpc.UnaryInterceptor(interceptor))
+
+	// init grpc Server
+	s := grpc.NewServer(opts...)
+
+	// register UserService
 	pb.RegisterUserServiceServer(s, userService)
 
 	fmt.Println("Listen on :58888")
