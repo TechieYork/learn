e6b5eccfcdbf936ed95f77fd95c94aa3f6038f06
york <york528@yeah.net>
2020-03-10 01:41:50 +0800

    fix:Remove binary file

---
+++ istio/samples/goods-grpc/gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./comments-grpc
+gofra kube deployment --override; gofra kube service --override; gofra istio virtual-service --override; gofra istio destination-rule --override
+
+cd ../goods-http
+gofra kube deployment --override; gofra kube service --override; gofra istio virtual-service --override; gofra istio destination-rule --override
+
+cd ../stock-grpc
+gofra kube deployment --override; gofra kube service --override; gofra istio virtual-service --override; gofra istio destination-rule --override
+
+cd ../user-grpc
+gofra kube deployment --override; gofra kube service --override; gofra istio virtual-service --override; gofra istio destination-rule --override
